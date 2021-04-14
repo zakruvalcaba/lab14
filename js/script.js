35ea18dffb1275e0cbc4e2af3db6d58b52ea1c8f
zakruvalcaba <zakruvalcaba@gmail.com>
2021-04-14 12:32:01 -0700

    updated lab and updated to ES6

--- js/script.js
+++ js/script.js
@@ -1,51 +1,63 @@
-/*eslint-env browser*/
+// GLOBAL VARIABLES
+let storage;
+let list;
+let task;
+let tasks = [];
 
-function getWords(text) {
-    "use strict";
-    var words;
-    //REMOVE ALL SPECIAL CHARACTERS FROM THE TEXT
-    text = text.replace(/\./g, "");
-    text = text.replace(/:/g, "");
-    text = text.replace(/,/g, "");
-    text = text.toLowerCase();
-    //CONVERT STRING TO ARRAY
-    words = text.split(" ");
-    //SORT THE ARRAY
-    words = words.sort();
-    return words;
-}
+// GET THE DOM ELEMENTS
+const $ = (id) => {
+    return document.getElementById(id);
+};
 
-function getUniqueWords(words) {
-    "use strict";
-    var i, uniqueWords = [];
-    //PUSH UNIQUE WORDS TO A NEW ARRAY AND RETURN IT
-    uniqueWords.push(words[0]);
-    for (i = 0; i < words.length; i += 1) {
-        if (words[i] !== words[i - 1]) {
-            uniqueWords.push(words[i]);
+// DISPLAY TASK LIST
+const displayTaskList = () => {
+    // IF THERE ARE NO TASKS IN ARRAY, CHECK STORAGE
+    if (tasks.length === 0) {
+        // GET TASKS FROM STORAGE OR EMPTY STRING IF STORAGE IS EMPTY
+        storage = localStorage.getItem('tasks') || '';
+        // IF NOT EMPTY, CONVERT TO ARRAY AND STORE IN TASKS VARIABLE
+        if (storage.length > 0) {
+            tasks = storage.split('|');
         }
     }
-    return uniqueWords;
-}
+    
+    // IF THERE ARE TASKS IN THE ARRAY...
+    // SORT AND CREATE TASKS STRING
+    if (tasks.length > 0) {
+        tasks.sort();
+        list = tasks.join('\n');
 
-function main() {
-    "use strict";
-    var text, words, uniqueWords, i;
-    
-    text = "With innovative approaches and advanced methodologies, Vecta Corporation provides scalable business solutions to help companies achieve success through revenue increase, cost management, and user satisfaction. Our approach stems from the three most important business growth aspects: helping companies reach prospects, assist in converting prospects to customers, and assist in retaining those customers. This is accomplished through our interactive solutions and expertise in providing a memorable and positive user experience.";
-    
-    //GET THE ARRAY OF WORDS MINUS SPECIAL CHARACTERS
-    words = getWords(text);
-    //GET AN ARRAY OF UNIQUE WORDS
-    uniqueWords = getUniqueWords(words);
-    //DISPLAY WORD AND UNIQUE WORD COUNT
-    window.console.log("The Wordlist Application\n\n");
-    window.console.log("Number of words: " + words.length);
-    window.console.log("Number of unique words: " + uniqueWords.length);
-    window.console.log("Word occurrences and count:");
-    //DISPLAY ALL OF THE WORDS AND LET GOOGLE DISPLAY COUNT
-    for (i = 0; i < words.length; i += 1) {
-        window.console.log(words[i] + "\n");
+        // DISPLAY TASKS STRING
+        $('task_list').value = list;
     }
-}
-main();
+};
+
+// ADD A TASK
+const addToTaskList = () => {
+    if ($('task').value !== '') {
+        // ADD TASK TO ARRAY
+        tasks.push($('task').value);
+        // ADD TASK TO STRORAGE
+        localStorage.setItem('tasks', tasks.join('|'));
+        // CLEAR THE TEXT BOX
+        $('task').value = '';
+        // SHOW UPDATED TASKS
+        displayTaskList();
+    } else {
+        alert('Please enter a task.');
+    }
+};
+
+// CLEAR TASK LIST
+const clearTaskList = () => {
+    tasks.length = 0;                   // EMPTY ARRAY
+    localStorage.removeItem('tasks');   // REMOVE TASKS FROM STORAGE
+    $('task_list').value = '';          // CLEAR OUT TASK LIST FIELD
+};
+
+// WIRE UP EVENT HANDLERS AND DISPLAY ANY TASKS IN LIST
+window.addEventListener('load', () => {
+    $('add_task').addEventListener('click', addToTaskList);
+    $('clear_tasks').addEventListener('click', clearTaskList);
+    displayTaskList();
+});
