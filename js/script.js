cae579885b0c841d2e859dbc9977f41b08d51628
Zak Ruvalcaba <zakruvalcaba@gmail.com>
2023-04-13 11:43:32 -0700

    Updated lab

--- js/script.js
+++ js/script.js
@@ -1,63 +1,78 @@
-// GLOBAL VARIABLES
-let storage;
-let list;
-let task;
-let tasks = [];
-
-// GET THE DOM ELEMENTS
-const $ = (id) => {
-    return document.getElementById(id);
-};
-
-// DISPLAY TASK LIST
-const displayTaskList = () => {
-    // IF THERE ARE NO TASKS IN ARRAY, CHECK STORAGE
-    if (tasks.length === 0) {
-        // GET TASKS FROM STORAGE OR EMPTY STRING IF STORAGE IS EMPTY
-        storage = localStorage.getItem('tasks') || '';
-        // IF NOT EMPTY, CONVERT TO ARRAY AND STORE IN TASKS VARIABLE
-        if (storage.length > 0) {
-            tasks = storage.split('|');
-        }
+function view(arrEmployees) {
+    // COUNTER THAT APPEARS NEXT TO EMPLOYEES NAME (START AT 1)
+    let i = 1
+    // LOOP THROUGH EMPLOYEES ARRAY
+    for (let employee of arrEmployees) {
+        console.log(`${i}. ${employee}`)
+        i++ // INCREMENT COUNTER
     }
-    
-    // IF THERE ARE TASKS IN THE ARRAY...
-    // SORT AND CREATE TASKS STRING
-    if (tasks.length > 0) {
-        tasks.sort();
-        list = tasks.join('\n');
-
-        // DISPLAY TASKS STRING
-        $('task_list').value = list;
-    }
-};
-
-// ADD A TASK
-const addToTaskList = () => {
-    if ($('task').value !== '') {
-        // ADD TASK TO ARRAY
-        tasks.push($('task').value);
-        // ADD TASK TO STRORAGE
-        localStorage.setItem('tasks', tasks.join('|'));
-        // CLEAR THE TEXT BOX
-        $('task').value = '';
-        // SHOW UPDATED TASKS
-        displayTaskList();
+    console.log('')
+}
+function add(arrEmployees) {
+    // COLLECT THE EMPLOYEE'S NAME AND TITLE
+    let name = prompt('Enter the employee\'s name')
+    let title = prompt('Enter the employee\'s title')
+    // PUSH NEW EMPLOYEE TO TEMP ARRAY
+    arrEmployees.push(`${name} (${title})`)
+    // DISPLAY SUCCESS MESSAGE
+    console.log(`${name} was successfully added.`)
+    console.log('')
+}
+function del(arrEmployees) {
+    // ASK USER WHICH EMPLOYEE THEY WANT TO DELETE
+    let num = parseInt(prompt('Enter employee number to delete'))
+    // CHECK AND MAKE SURE ENTRY IS A VALID NUMBER
+    if (num < 1 || num > arrEmployees.length) {
+        alert('Invalid employee number.')
     } else {
-        alert('Please enter a task.');
+        let employee = arrEmployees.splice(num - 1, 1)
+        console.log(`${employee} was successfully deleted.`)
+        console.log('')
     }
-};
-
-// CLEAR TASK LIST
-const clearTaskList = () => {
-    tasks.length = 0;                   // EMPTY ARRAY
-    localStorage.removeItem('tasks');   // REMOVE TASKS FROM STORAGE
-    $('task_list').value = '';          // CLEAR OUT TASK LIST FIELD
-};
-
-// WIRE UP EVENT HANDLERS AND DISPLAY ANY TASKS IN LIST
-window.addEventListener('load', () => {
-    $('add_task').addEventListener('click', addToTaskList);
-    $('clear_tasks').addEventListener('click', clearTaskList);
-    displayTaskList();
-});
+}
+function init() {
+    // DISPLAY COMMAND MENU
+    console.log('The Employee Management Application')
+    console.log('-----------------------------------')
+    console.log('COMMAND MENU')
+    console.log('show - Show all employees')
+    console.log('add - Add an employee')
+    console.log('del - Delete an employee')
+    console.log('exit - Exit the application')
+    console.log('-----------------------------------')
+    console.log('')
+    // START WITH AN EMPTY ARRAY
+    let arrEmployees = []
+    // FETCH THE JSON DATA
+    fetch('../data/employees.json')
+        .then( response => response.json() )
+        .then( data => {
+            for (let employee of data.employees) {
+                arrEmployees.push(`${employee.name} (${employee.title})`)
+            }
+            // KEEP THE USER AT THE COMMAND MENU
+            do {
+                // ALLOW THE USER TO ENTER A COMMAND
+                let command = prompt('Enter command').toLowerCase()
+                // CHECK AND MAKE SURE COMMAND IS NOT NULL
+                if (command !== null) {
+                    if (command === 'show') {
+                        view(arrEmployees)
+                    } else if (command === 'add') {
+                        add(arrEmployees)
+                    } else if (command === 'del') {
+                        del(arrEmployees)
+                    } else if (command === 'exit') {
+                        break
+                    } else {
+                        alert('That is not a valid command.')
+                    }
+                } else {
+                    alert('Please enter a command.')
+                }
+            } while (true)
+            console.log('The program has been terminated.')
+        })
+        .catch( error => console.log(error.message) )
+}
+init()
